8ffeed4ea8bf73c9f745310fb3182bf2fb86864b
marceladacruz779-sys <marceladacruz779@gmail.com>
2026-03-09 07:33:15 -0300

    get2

--- API-musica.js
+++ API-musica.js
@@ -1,14 +1,11 @@
-// Importar o framework Express
 const express = require('express');
 
-// Criar a instância da aplicação
 const app = express();
 
-// MIDDLEWARE: permite ler JSON no body das requisições
-// Sem isso, req.body sempre será undefined no POST/PUT
+
 app.use(express.json());
 
-// Cria uma Array de objetos (simulando um banco de dados) com ID
+
 let musicas = [
     { id: 1, nome: 'Yellowstone Theme', autor: 'Yellowstone (Original Television Series Soundtrack)', link: 'https://www.youtube.com/watch?v=YzX1jPQNbYQ&list=PLRW80bBvVD3U1tL2L7LiXPabRcZCYkzxM&index=1' },
     { id: 2, nome: 'Valley of the Soul', autor: 'Yellowstone (Original Television Series Soundtrack)', link: 'https://www.youtube.com/watch?v=EATvpGpLa_8&list=PLRW80bBvVD3U1tL2L7LiXPabRcZCYkzxM&index=17' },
@@ -16,15 +13,23 @@ let musicas = [
     { id: 4, nome: 'Buryinf Secrets', autor: 'Yellowstone (Original Television Series Soundtrack)', link: 'https://www.youtube.com/watch?v=CaSXKX9GiPc&list=PLRW80bBvVD3U1tL2L7LiXPabRcZCYkzxM&index=16' }
 ];
 
-// GET /produtos — Retorna TODOS os produtos
+
 app.get('/musicas', (req, res) => {
-// Status 200 = OK (sucesso)
 res.status(200).json(musicas);
 });
 
+app.get('/musicas/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const musica = musicas.find(p => p.id === id);
+
+    if (musica) {
+    res.status(200).json(musica);
+    } else {
+    res.status(404).json({mensagem: "Produto não encontrado"});
+    }
+});
 
 
-// Servidor
 app.listen(3000, () => {
     console.log(`🚀 Servidor rodando em http://localhost:3000`);
     console.log(`📋 Acesse http://localhost:3000/musicas`);
